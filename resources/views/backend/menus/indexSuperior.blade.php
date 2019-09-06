090f3d287d1556dd06c73f177290f6d92aec1349
Jonathan Moran <tatangamess@gmail.com>
2019-09-06 09:02:27 -0600

    modificando siempre

--- resources/views/backend/menus/indexSuperior.blade.php
+++ resources/views/backend/menus/indexSuperior.blade.php
@@ -3,16 +3,12 @@
 <head>
 	<title>Acceso</title>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    @yield('content-admin-css')
 
-	<!-- libreria fuentes adminlte3 -->
-	<link href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}" type="text/css" rel="stylesheet" />
-	<!-- libreria estilos adminlte3 -->
-    <link href="{{ asset('css/backend/adminlte3/adminlte.min.css') }}" type="text/css" rel="stylesheet" />  
-    <!-- Google Font: Source Sans Pro -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-	
-	<link href="{{ asset('css/backend/adminlte3/dataTables.bootstrap4.css') }}" type="text/css" rel="stylesheet" />  
+    
+
 	
 </head>
   <body class="hold-transition sidebar-mini">
