7ef7d82c630562f26d84d893176d8224d66cc7ff
giovanyrosales <gioer21@gmail.com>
2019-09-06 12:24:01 -0600

    modificacion footer

--- resources/views/backend/menus/footer.blade.php
+++ resources/views/backend/menus/footer.blade.php
@@ -13,8 +13,8 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-      Anything you want
+      Panel de Control
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2019 <a href="#">Alcaldia Municipal de Metap&aacute;n</a>.</strong> Derechos Reservados.
   </footer>
