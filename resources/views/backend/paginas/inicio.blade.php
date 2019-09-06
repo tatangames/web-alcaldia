3d9c0d6e67aea8e9444224768aa7a4343c72cbdb
Alexander15164 <alexandermm2011@gmail.com>
2019-09-06 11:44:08 -0600

    Vista resumen de publicaciones en pagina pricipal

--- resources/views/backend/paginas/inicio.blade.php
+++ resources/views/backend/paginas/inicio.blade.php
@@ -1,122 +1,120 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-	<title>Acceso</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Panel de control</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- libreria fuentes adminlte3 -->
-	<link href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}" type="text/css" rel="stylesheet" />
-	<!-- libreria estilos adminlte3 -->
-    <link href="{{ asset('css/backend/adminlte3/adminlte.min.css') }}" type="text/css" rel="stylesheet" />  
-    <!-- Google Font: Source Sans Pro -->
-	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-	
-	<link href="{{ asset('css/backend/adminlte3/dataTables.bootstrap4.css') }}" type="text/css" rel="stylesheet" />  
-	
+  <!-- libreria fuentes adminlte3 -->
+  <link href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}" type="text/css" rel="stylesheet" />
+  <!-- libreria estilos adminlte3 -->
+  <link href="{{ asset('css/backend/adminlte3/adminlte.min.css') }}" type="text/css" rel="stylesheet" />
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+
+  <link href="{{ asset('css/backend/adminlte3/dataTables.bootstrap4.css') }}" type="text/css" rel="stylesheet" />
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
 </head>
-  <body class="hold-transition sidebar-mini">
-    
- 
 
-   <!-- Content Wrapper. Contains page content -->
- 
-    <!-- Content Header (Page header) -->
+<body class="hold-transition sidebar-mini">
+  
+
+  <!-- Main content -->
+  <div class="wrapper">
     <div class="content-header">
       <div class="container-fluid">
-        <div class="row mb-2">
+        <div class="row mb-12">
           <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Starter Page</h1>
+            <h1 class="m-0 text-dark">Pantalla Principal</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
+              <li class="breadcrumb-item"><a href="#">Inicio</a></li>
+              <li class="breadcrumb-item active">Resumen de publicaciones</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
-    <!-- /.content-header -->
 
-    <!-- Main content -->
-    <div class="content">
+    <section class="content">
       <div class="container-fluid">
+        <!-- Small boxes (Stat box) -->
         <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner">
+                <h3>0</h3>
 
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+                <p>Porgramas Municipales</p>
               </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
+              <div class="icon">
+                <i class="ion ion-ios-paper"></i>
               </div>
-            </div><!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
+              <a class="small-box-footer"><i class="icon ion-pie-graph"></i></a>
             </div>
           </div>
-          <!-- /.col-md-6 -->
+
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>Servicios Municipales</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-ios-briefcase"></i>
+              </div>
+              <a class="small-box-footer"><i class="icon ion-pie-graph"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+          <div class="col-lg-4 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+              <div class="inner">
+                <h3>0</h3>
+
+                <p>Noticias </p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-ios-paper"></i>
+              </div>
+              <a class="small-box-footer"><i class="icon ion-pie-graph"></i></a>
+            </div>
+          </div>
+          <!-- ./col -->
+
+
         </div>
         <!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content -->
- 
-  <!-- /.content-wrapper -->
+        <!-- Main row -->
+        <div class="row">
+          <!-- Left col -->
+        </div>
+        <!-- /.content -->
+
+        <!-- /.content-wrapper -->
+      </div>
+    </section>
+  </div>
 
 
-	
-	<!-- libreria jquery -->
-	<script src="{{ asset('plugins/jquery/jquery.min.js') }}" type="text/javascript"></script>
-	<!-- libreria bootstrap4 -->
-	<script src="{{ asset('plugins/bootstrap/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
-	<!-- libreria adminlte3 -->
-	<script src="{{ asset('js/backend/adminlte3/adminlte.min.js') }}" type="text/javascript"></script>
+  <!-- libreria jquery -->
+  <script src="{{ asset('plugins/jquery/jquery.min.js') }}" type="text/javascript"></script>
+  <!-- libreria bootstrap4 -->
+  <script src="{{ asset('plugins/bootstrap/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+  <!-- libreria adminlte3 -->
+  <script src="{{ asset('js/backend/adminlte3/adminlte.min.js') }}" type="text/javascript"></script>
 
-	@yield('content-admin-js')
+  @yield('content-admin-js')
 
 </body>
-</html>
 
+</html>
