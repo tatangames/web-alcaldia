b50c0f65bf615d2d0835984ba4484c6081114c6f
Jonathan Moran <tatangamess@gmail.com>
2019-09-06 10:28:38 -0600

    modificando x

--- resources/views/backend/menus/indexInferior.blade.php
+++ resources/views/backend/menus/indexInferior.blade.php
@@ -1,11 +1,16 @@
+
+
+
 <!-- libreria jquery -->
 <script src="{{ asset('plugins/jquery/jquery.min.js') }}" type="text/javascript"></script>
 	<!-- libreria bootstrap4 -->
-	<script src="{{ asset('plugins/bootstrap/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('plugins/bootstrap/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
+
 	<!-- libreria adminlte3 -->
-	<script src="{{ asset('js/backend/adminlte3/adminlte.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/backend/adminlte3/adminlte.min.js') }}" type="text/javascript"></script>
+  
+    
 
-	
-
+    @yield('content-admin-js')
 </body>
 </html>
--- resources/views/backend/paginas/inicio.blade.php
+++ resources/views/backend/paginas/inicio.blade.php
@@ -107,13 +107,7 @@
   <!-- /.content-wrapper -->
 
 
-	
-	<!-- libreria jquery -->
-	<script src="{{ asset('plugins/jquery/jquery.min.js') }}" type="text/javascript"></script>
-	<!-- libreria bootstrap4 -->
-	<script src="{{ asset('plugins/bootstrap/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
-	<!-- libreria adminlte3 -->
-	<script src="{{ asset('js/backend/adminlte3/adminlte.min.js') }}" type="text/javascript"></script>
+
 
 	@yield('content-admin-js')
 
