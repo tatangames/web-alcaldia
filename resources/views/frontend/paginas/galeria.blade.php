198dfbac1a1e0c0f7f93214fbc26bd5b2a46e409
= <=>
2019-09-17 12:30:41 -0600

    merge

--- resources/views/frontend/paginas/galeria.blade.php
+++ resources/views/frontend/paginas/galeria.blade.php
@@ -4,12 +4,11 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>{{ $programa->nombreprograma }}</title>
+	<title>Galer&iacute;a de fotos</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="" />
 	<meta name="keywords" content="" />
 	<meta name="author" content="" />
-
 
 	<link href='{{ asset('images/LOGO_2_-_copia.png') }}' rel='shortcut icon' type='image/png'>
 	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
@@ -33,7 +32,7 @@
 				<div class="container-fluid ">
 					<div class="row">
 						<div class="col-xs-2">
-							<div id="colorlib-logo"><a href="index.html"><img src="{{ asset('images/LOGO_2_-_copia.png') }}" alt=""
+							<div id="colorlib-logo"><a href="index.html"><img src="images/LOGO_2_-_copia.png" alt=""
 										srcset="" width="60px" height="80px;"
 										style="position: relative; top:-25px;"></a></div>
 						</div>
@@ -58,27 +57,27 @@
 								</li>
 								<li><a href="/galeria">Galer&iacute;a</a></li>
 								<li><a href="#colorlib-footer" class="ancla" data-ancla="colorlib-footer">Contacto</a></li>
-								<li><a href="https://www.transparencia.gob.sv/institutions/alc-metapan">Portal de transparencia</a></li>
+								<li><a href="https://www.transparencia.gob.sv/institutions/alc-metapan">Portal de
+										transparencia</a></li>
 							</ul>
 						</div>
 					</div>
 				</div>
 			</div>
 		</nav>
-		<!--End Barra de navegacion-->
+		<!--End Barra de navegacion -->
 
-		<!--Imagen de cabecera-->
+		<!--Imagen de Cabecera-->
 		<aside id="colorlib-hero">
 			<div class="flexslider">
 				<ul class="slides">
-					<li style="background-image: url(../images/Slider/a4.jpg);">
+					<li style="background-image: url(images/Slider/a2.jpg);">
 						<div class="overlay"></div>
 						<div class="container-fluid">
 							<div class="row">
-								<div class="col-md-6 col-sm-12 col-md-offset-3 slider-text">
+								<div class="col-md-6 col-md-offset-3 col-sm-12 col-xs-12 slider-text">
 									<div class="slider-text-inner text-center">
-										<h1>{{ $programa->nombreprograma }}</h1>
-
+										<h1>Galer&iacute;a de Fotos</h1>
 									</div>
 								</div>
 							</div>
@@ -87,36 +86,57 @@
 				</ul>
 			</div>
 		</aside>
-		<!--End Imagen de cabecera-->
+		<!--End Imagen de Cabecera-->
 
-		<h5>.</h5>
-		<!--Contenido-->
-		<div id="colorlib-services">
+		<!--Galeria de fotos-->
+		<div class="colorlib-wrap">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-12 text-center">
-						<h1>Informacion Programa</h1>
-					</div>
-					<br><br><br>
-					<div class="row">
-						<div class="col-md-4">
-							<center>
-							<img src="../storage/programa/{{ $programa->logo }}" alt="Programa Municipal" style="width:300px; height:300px;"/>
-							</center>
+					<div class="col-md-12">
+						<div class="row">
+							<div class="wrap-division">
+							@foreach($fotografias as $foto)
+								<div class="col-md-4 col-sm-4 animate-box">
+									<div class="tour">
+										<a class="tour-img" style="background-image: url(storage/noticia/{{ $foto->nombrefotografia }});"
+											data-toggle="modal" data-target="#modal1" onclick="getPath(this)"></a>
+									</div>
+								</div>
+								@endforeach
+								
+							</div>
 						</div>
-						<div class="col-md-8">
-						<center>{!! $programa->deslarga  !!} </center>
-						</div>
+						<br><br>
 					</div>
-
 				</div>
-				<br><br>
 			</div>
 		</div>
-		<!--End Contenido-->
+		<!--End galería de fotos-->
+
+		<!--Cuadro modal Zoom  fotos-->
+		<div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+			aria-hidden="true">
+			<div class="modal-dialog modal-lg" role="document">
+				<!--Contenido-->
+				<div class="modal-content">
+					<div class="modal-body mb-0 p-0">
+						<div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
+							<img id="imgModal" src="images/Slider/a1.jpg" class="embed-responsive-item" alt="">
+						</div>
+					</div>
+					<!--Pie de pagina -->
+					<div class="modal-footer justify-content-center">
+						<button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4"
+							data-dismiss="modal">Cerrar</button>
+					</div>
+				</div>
+				<!--Fin Contenido-->
+			</div>
+		</div>
+		<!--End Cuadro modal-->
 
 		<!--Imagen pie de pagina-->
-		<div id="colorlib-subscribe" style="background-image: url(../images/Slider/a5.jpg);"
+		<div id="colorlib-subscribe" style="background-image:url(images/Slider/a5.jpg);"
 			data-stellar-background-ratio="0.5">
 			<div class="overlay"></div>
 			<div class="container">
@@ -141,53 +161,59 @@
 
 		<!--Pie de pagina-->
 		<footer id="colorlib-footer" role="contentinfo">
-		<div class="container">
-			<div class="row row-pb-md">
-				<div class="col-md-3 colorlib-widget">
-					<h4>Siguenos en nuestras redes</h4>
-					<p></p>
-					<p>
-						<ul class="colorlib-social-icons">
-							<li><a href="https://www.facebook.com/AlcaldiadeMetapan/"><i class="icon-facebook"></i></a></li>				
-							<li><a href="https://twitter.com/Alcaldia_Met"><i class="icon-twitter"></i></a></li>
-							<li><a href="https://www.instagram.com/alcaldiademetapan/"><i class="icon-instagram"></i></a></li>
-							<li><a href="https://www.youtube.com/channel/UCWSEAyHR42uZHCY3eWW-ELA"><i class="icon-youtube"></i></a></li>
+			<div class="container">
+				<div class="row row-pb-md">
+					<div class="col-md-3 colorlib-widget">
+						<h4>Siguenos en nuestras redes</h4>
+						<p></p>
+						<p>
+							<ul class="colorlib-social-icons">
+								<li><a href="https://www.facebook.com/AlcaldiadeMetapan/"><i
+											class="icon-facebook"></i></a></li>
+								<i><a href="https://www.youtube.com/channel/UCWSEAyHR42uZHCY3eWW-ELA"><i
+											class="icon-youtube"></i></a></i>
+							</ul>
+						</p>
+					</div>
+					<div class="col-md-3 col-md-push-1">
+						<h4>Contactanos!</h4>
+						<ul class="colorlib-footer-links">
+							<li>Avenida Benjam&iacute;n Estrada Valiente<br> 1ra. Calle Poniente, Barrio San
+								Pedro,<br>Metap&aacute;n, Santa Ana</li>
+							<li><a href="tel://24027615">2402-7615</a></li>
+							<li><a href="tel://24027601">2402-7601</a></li>
 						</ul>
-					</p>
+					</div>
 				</div>
-				<div class="col-md-3 col-md-push-1">
-					<h4>Contactanos!</h4>
-					<ul class="colorlib-footer-links">
-						<li>Avenida Benjam&iacute;n Estrada Valiente<br> 1ra. Calle Poniente, Barrio San
-							Pedro,<br>Metap&aacute;n, Santa Ana</li>
-						<li><i class="icon-phone"></i><a href="tel://24027615">  2402-7615</a></li>
-						<li><i class="icon-phone"></i><a href="tel://24027601">  2402-7601</a></li>
-					</ul>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12 text-center">
-					<p>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;
-						<script>
-							document.write(new Date().getFullYear());
-						</script> Todos los derechos reservados |
-						Alclad&iacute;a Municipal de Metap&aacute;n, Santa Ana, El Salvador.
-					</p>
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<p>
+							Copyright &copy;
+							<script>document.write(new Date().getFullYear());</script> Todos los derechos reservados |
+							Alclad&iacute;a Municipal de Metap&aacute;n, Santa Ana, El Salvador.
+						</p>
+					</div>
 				</div>
 			</div>
-		</div>
-	</footer>
+		</footer>
 		<!--End pie de pagina-->
 	</div>
+	<!--End pie de pagina-->
 
 	<!--Boton subir-->
 	<div class="gototop js-top">
-		<a  class="js-gotop"><i class="icon-arrow-up2"></i></a>
+		<a class="js-gotop"><i class="icon-arrow-up2"></i></a>
 	</div>
-	<!--End Boton subir-->
+	<!--End boton subir-->
 
+	<!-- Metodo cambiar url cuadro modal-->
+	<script type="text/javascript">
+		function getPath(img) {
+			atributo = img.style.backgroundImage;
+			document.getElementById("imgModal").setAttribute("src", atributo.substr(5, atributo.length - 7))
+		}
+	</script>
+	<!--Fin Metodo url-->
 	<script src="{{ asset('js/frontend.js') }}" type="text/javascript"></script>
 
 	<script>
