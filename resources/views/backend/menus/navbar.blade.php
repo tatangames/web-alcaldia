adb7fa6c3a2701b5203e91872fa981a6a58bf648
giovanyrosales <gioer21@gmail.com>
2019-09-24 16:05:10 -0600

    quite inicio y contacto de admin index

--- resources/views/backend/menus/navbar.blade.php
+++ resources/views/backend/menus/navbar.blade.php
@@ -5,7 +5,6 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
       </li>
-      
     </ul>
 
     <!-- Right navbar links -->
