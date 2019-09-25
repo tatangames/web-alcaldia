1e8236ab149bab55b948fe6faebf96aa1436a350
Jonathan Moran <tatangamess@gmail.com>
2019-09-25 09:22:30 -0600

    libreria adminlte movida

--- resources/views/backend/menus/indexSuperior.blade.php
+++ resources/views/backend/menus/indexSuperior.blade.php
@@ -8,7 +8,7 @@
 	<!-- libreria fuentes adminlte3 -->
 	<link href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}" type="text/css" rel="stylesheet" />
 	<!-- libreria estilos adminlte3 -->
-    <link href="{{ asset('css/backend/adminlte3/adminlte.min.css') }}" type="text/css" rel="stylesheet" />  
+    
     <!-- Google Font: Source Sans Pro -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 	
