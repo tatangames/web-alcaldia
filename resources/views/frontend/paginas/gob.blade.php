a6da3bbeaa6226e935afea887d47aa8e88f52eb7
Alexander Monzon <alexandermm2011@gmail.com>
2019-09-25 09:27:33 -0600

    Agregando contenido a la pagina gobierno municipal, class responsive imagenes en pagina historia

--- resources/views/frontend/paginas/gob.blade.php
+++ resources/views/frontend/paginas/gob.blade.php
@@ -1,5 +1,6 @@
 <!--Parte superior de las paginas -  hasta head  -->
 @include('frontend.menus.indexSuperior')
+
 <body>
 	<div class="colorlib-loader"></div>
 	<div id="page">
@@ -34,17 +35,92 @@
 			<div class="container">
 				<div class="row">
 					<div class="about-flex">
-						<div class="col-one-forth aside-stretch animate-box">
+						<div class="col-md-1 aside-stretch animate-box">
 							<div class="row">
-								<div class="col-md-12 about">
-
-								</div>
 							</div>
 						</div>
-						<div class="col-three-forth animate-box">
+						<div class="col-three-forth animate-box text-center">
 							<h2>Nuestro Gobierno</h2>
-							<p>...</p>
+							<div class="row">
+								<div class="col-md-12">		
+									<img src="{{asset('images/historia/IMG_4348.jpg')}}" alt="Alcalde y Consejo" class="img-responsive" width="900" ></p>
+								</div>
+							</div>
+									<br>
+									<p style="text-align: center;"><strong style="font-size: 20px;">NOMINA DE CONCEJALES 2018-2021</strong></p>
 
+									<table class="table table-hover table-bordered">
+										<tbody>
+											<tr>
+												<td>
+													<strong>Alcalde Municipal </strong>
+												</td>
+												<td><span>Prof. José Rigoberto Pinto Rivera</span></td>
+											</tr>
+											<tr>
+												<td>
+													<strong>Síndico</strong>
+												</td>
+												<td><span>Lic. Ramón Alberto Calderón</span></td>
+											</tr>
+											<tr>
+												<td><strong>1° Regidor Propietario</strong></td>
+												<td><span>José Roberto Lemus Morataya</span></td>
+											</tr>
+											<tr>
+												<td><strong>2° Regidor Propietaria</strong></td>
+												<td><span>Sr. Pedro Antonio Sanabria</span></td>
+											</tr>
+											<tr>
+												<td><strong>3° Regidora Propietario</strong></td>
+												<td><span>Sra. Nora Elizabeth Hernández</span></td>
+											</tr>
+											<tr>
+												<td><strong>4° Regidor Propietario</strong></td>
+												<td><span>Sr. Rudy Alfredo Sanabria</span></td>
+											</tr>
+											<tr>
+												<td><strong>5° Regidor Propietaria</strong></td>
+												<td><span>Sr. Alejandro Lemus M.</span></td>
+											</tr>
+											<tr>
+												<td><strong>6° Regidor Propietario</strong></td>
+												<td><span>Lic. José Atilio Granados</span></td>
+											</tr>
+											<tr>
+												<td><strong>7° Regidor Propietario</strong></td>
+												<td><span></span>Sr. Julio Enrique Martínez</td>
+											</tr>
+											<tr>
+												<td><strong>8° Regidor Propietaria</strong></td>
+												<td><span>Sr. José Misael Posada</span></td>
+											</tr>
+											<tr>
+												<td><strong>9° Regidor Propietario</strong></td>
+												<td><span>Lic. Ricardo Alberto Polanco</span></td>
+											</tr>
+											<tr>
+												<td><strong>10° Regidor Propietario</strong></td>
+												<td><span>Ing. Nelson Eduardo Figueroa</span></td>
+											</tr>
+											<tr>
+												<td><strong>1° Regidor Suplente</strong></td>
+												<td><span>Sr. Carlos Armando Sandoval</span></td>
+											</tr>
+											<tr>
+												<td><strong>2° Regidor Suplente</strong></td>
+												<td><span>Sr. Ricardo Pacheco Pacheco</span></td>
+											</tr>
+											<tr>
+												<td><strong>3° Regidor Suplente</strong></td>
+												<td><span>Sra. Nora Elizabeth Hernández</span></td>
+											</tr>
+											<tr>
+												<td><strong>4° Regidor Suplente</strong></td>
+												<td><span>Sr. Rudy Alfredo Sanabria</span></td>
+											</tr>
+										</tbody>
+									</table>
 						</div>
 					</div>
 				</div>
@@ -53,17 +129,19 @@
 		<!--End contenido-->
 
 		@include("frontend.menus.footer")
-	<script src="{{ asset('js/frontend.js') }}" type="text/javascript"></script>
+		<script src="{{ asset('js/frontend.js') }}" type="text/javascript"></script>
 
-	<script>
-  		$(document).ready(function(){
-   			$(".ancla").click(function(evento){
-      		evento.preventDefault();
-      		var codigo = "#" + $(this).data("ancla");
-      		$("html,body").animate({scrollTop: $(codigo).offset().top}, 300);
-    		});
-  		});
-</script>
+		<script>
+			$(document).ready(function() {
+				$(".ancla").click(function(evento) {
+					evento.preventDefault();
+					var codigo = "#" + $(this).data("ancla");
+					$("html,body").animate({
+						scrollTop: $(codigo).offset().top
+					}, 300);
+				});
+			});
+		</script>
 
 </body>
 
