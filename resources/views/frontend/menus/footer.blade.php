4f8b24a6d2d02ad565a745abdecb46db49f90654
Alexander Monzon <alexandermm2011@gmail.com>
2019-09-26 10:57:50 -0600

    arreglando bug en div mapa index

--- resources/views/frontend/menus/footer.blade.php
+++ resources/views/frontend/menus/footer.blade.php
@@ -1,67 +1,67 @@
 <!--Imagen del footer-->
 <div id="colorlib-subscribe" style="background-image: url({{ asset('/images/Slider/a5.jpg') }});" data-stellar-background-ratio="0.5">
-		<div class="overlay"></div>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
-					<h2>Alcald&iacute;a Municipal de Metapán</h2>
+	<div class="overlay"></div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3 text-center colorlib-heading animate-box">
+				<h2>Alcald&iacute;a Municipal de Metapán</h2>
 
-					<form class="form-inline qbstp-header-subscribe">
-						<div class="row">
-							<div class="col-md-12 col-md-offset-0">
-								<div class="form-group">
+				<form class="form-inline qbstp-header-subscribe">
+					<div class="row">
+						<div class="col-md-12 col-md-offset-0">
+							<div class="form-group">
 
-								</div>
 							</div>
 						</div>
-					</form>
-				</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</div>
-	<!--Pie de pagina-->
-	<footer id="colorlib-footer" role="contentinfo">
-		<div class="container">
-			<div class="row row-pb-md">
-				<div class="col-md-3 colorlib-widget">
-					<h4>Siguenos en nuestras redes</h4>
-					<p></p>
-					<p>
-						<ul class="colorlib-social-icons">
-							<li><a href="https://www.facebook.com/AlcaldiadeMetapan/"><i class="icon-facebook"></i></a></li>				
-							<li><a href="https://twitter.com/Alcaldia_Met"><i class="icon-twitter"></i></a></li>
-							<li><a href="https://www.instagram.com/alcaldiademetapan/"><i class="icon-instagram"></i></a></li>
-							<li><a href="https://www.youtube.com/channel/UCWSEAyHR42uZHCY3eWW-ELA"><i class="icon-youtube"></i></a></li>
-						</ul>
-					</p>
-				</div>
-				<div class="col-md-3 col-md-push-1">
-					<h4>Contactanos!</h4>
-					<ul class="colorlib-footer-links">
-						<li>Avenida Benjam&iacute;n Estrada Valiente<br> 1ra. Calle Poniente, Barrio San
-							Pedro,<br>Metap&aacute;n, Santa Ana</li>
-						<li><i class="icon-phone"></i><a href="tel://24027615">  2402-7615</a></li>
-						<li><i class="icon-phone"></i><a href="tel://24027601">  2402-7601</a></li>
+</div>
+<!--Pie de pagina-->
+<footer id="colorlib-footer" role="contentinfo">
+	<div class="container">
+		<div class="row row-pb-md">
+			<div class="col-md-3 colorlib-widget">
+				<h4>Siguenos en nuestras redes</h4>
+				<p></p>
+				<p>
+					<ul class="colorlib-social-icons">
+						<li><a href="https://www.facebook.com/AlcaldiadeMetapan/"><i class="icon-facebook"></i></a></li>
+						<li><a href="https://twitter.com/Alcaldia_Met"><i class="icon-twitter"></i></a></li>
+						<li><a href="https://www.instagram.com/alcaldiademetapan/"><i class="icon-instagram"></i></a></li>
+						<li><a href="https://www.youtube.com/channel/UCWSEAyHR42uZHCY3eWW-ELA"><i class="icon-youtube"></i></a></li>
 					</ul>
-				</div>
+				</p>
 			</div>
-			<div class="row">
-				<div class="col-md-12 text-center">
-					<p>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;
-						<script>
-							document.write(new Date().getFullYear());
-						</script> Todos los derechos reservados |
-						Alcald&iacute;a Municipal de Metap&aacute;n, Santa Ana, El Salvador.
-					</p>
-				</div>
+			<div class="col-md-3 col-md-push-1">
+				<h4>Contactanos!</h4>
+				<ul class="colorlib-footer-links">
+					<li>Avenida Benjam&iacute;n Estrada Valiente<br> 1ra. Calle Poniente, Barrio San
+						Pedro,<br>Metap&aacute;n, Santa Ana</li>
+					<li><i class="icon-phone"></i><a href="tel://24027615"> 2402-7615</a></li>
+					<li><i class="icon-phone"></i><a href="tel://24027601"> 2402-7601</a></li>
+				</ul>
 			</div>
 		</div>
-	</footer>
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<p>
+					<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+					Copyright &copy;
+					<script>
+						document.write(new Date().getFullYear());
+					</script> Todos los derechos reservados |
+					Alcald&iacute;a Municipal de Metap&aacute;n, Santa Ana, El Salvador.
+				</p>
+			</div>
+		</div>
 	</div>
-	<!--Icono subir-->
-	<div class="gototop js-top">
-		<a class="js-gotop"><i class="icon-arrow-up2"></i></a>
-	</div>
-	<!--Fin icono sunbir-->
+</footer>
+
+<!--Icono subir-->
+<div class="gototop js-top">
+	<a class="js-gotop"><i class="icon-arrow-up2"></i></a>
+</div>
+<!--Fin icono sunbir-->
