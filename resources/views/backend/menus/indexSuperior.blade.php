082757b60989d30b4da0aeca536867e569a8ca7a
giovanyrosales <gioer21@gmail.com>
2024-07-17 15:16:26 -0600

    actualice nombre tab

--- resources/views/backend/menus/indexSuperior.blade.php
+++ resources/views/backend/menus/indexSuperior.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es" class="scrollbar-deep-purple thin square">
 <head>
-	<title>Alcaldia Metapan - Panel</title>
+	<title>Santa Ana Norte - Panel</title>
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
